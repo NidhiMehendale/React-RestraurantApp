add983eceb8bbcbbdd997c9254a62882267e4fcc
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-06-08 16:26:55 +0530

    Adding Header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <MealsSummary/>
+    </Fragment>
+  );
+}
+
+export default App;
