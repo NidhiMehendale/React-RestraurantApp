13815c9669c4e98c83ccab0b0e45aa04fafc3833
Dipti Mehendale <diptimehendale@Diptis-MacBook-Air.local>
2023-06-09 15:28:02 +0530

    Create hard Coded Modal

---
+++ src/components/UI/Modal.js
@@ -0,0 +1,32 @@
+import classes from './Modal.module.css';
+import ReactDOM  from 'react-dom';
+import { Fragment } from 'react';
+
+const Backdrop = (props) => {
+    return <div className={classes.backdrop} />
+};
+
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+         <div className={classes.content}>{props.children}</div>
+        </div>
+    );
+};
+
+const portelElement = document.getElementById('overlays');
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+       {ReactDOM.createPortal(<Backdrop />, portelElement)};
+       {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay> , 
+        portelElement
+        )};
+    </Fragment>
+  );
+
+};
+
+export default Modal;
